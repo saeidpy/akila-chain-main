08f9ef813729a2dcd876ab99f91c61b2f2eb77e7
saeidpy <saeedpy@gamil.com>
2022-07-17 18:56:54 +0430

    fix window condition

--- apps/frontend/lib/api.js
+++ apps/frontend/lib/api.js
@@ -6,7 +6,7 @@ import qs from "qs";
  */
 export function getURL(path = "") {
   return `${
-    window === "undefined"
+    typeof window === "undefined"
       ? process.env.NEXT_PUBLIC_API_URL_SERVER
       : process.env.NEXT_PUBLIC_API_URL_CLIENT || "http://localhost:1337"
   }${path}`;
