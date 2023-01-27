d070c7f71c7da6d95d12379297741bfc45e87b00
saeidpy <saeedpy@gamil.com>
2023-01-27 20:42:25 +0330

    fix global state

--- apps/frontend/pages/_app.js
+++ apps/frontend/pages/_app.js
@@ -17,15 +17,6 @@ const theme = {
   mobile: "576px",
 };
 
-export async function getServerSideProps({ params }) {
-  // Fetch global site settings from
-  const globalRes = await fetchAPI("/global", {
-    populate: "*",
-  });
-  // Pass the data to our page via props
-  return { props: { global: globalRes?.data ?? {} } };
-}
-
 // Store Global object in context
 export const GlobalContext = createContext({});
 export default function MyApp({ Component, pageProps }) {
@@ -65,16 +56,18 @@ export default function MyApp({ Component, pageProps }) {
 // have getStaticProps. So article, category and home pages still get SSG.
 // Hopefully we can replace this with getStaticProps once this issue is fixed:
 // https://github.com/vercel/next.js/discussions/10949
-// MyApp.getInitialProps = async (ctx) => {
-//   // Calls page's `getInitialProps` and fills `appProps.pageProps`
-//   const appProps = await App.getInitialProps(ctx);
-//   // Fetch global site settings from Strapi
-//   const globalRes = await fetchAPI("/global", {
-//     populate: "*",
-//   });
-//   // Pass the data to our page via props
-//   return { ...appProps, pageProps: { global: globalRes?.data ?? {} } };
-// };
+
+//IMPORTANT THIS FUNCTION IS TOTALLY CORRECT BECAUSE OF WE NEED GLOBAL STATE IN ALL COMPONENTS
+MyApp.getInitialProps = async (ctx) => {
+  // Calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(ctx);
+  // Fetch global site settings from Strapi
+  const globalRes = await fetchAPI("/global", {
+    populate: "*",
+  });
+  // Pass the data to our page via props
+  return { ...appProps, pageProps: { global: globalRes?.data ?? {} } };
+};
 
 const Root = styled.div`
   position: relative;
